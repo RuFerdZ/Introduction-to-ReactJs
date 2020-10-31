f954c441c7d071dd7648804a0268272a7a62bc4e
ruferdz <ge.rusiru@gmail.com>
2020-10-31 18:57:50 +0530

    basics to reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [users, setUsers] = useState([
+    { name: "Rusiru Fernando", message: "Hello im working at Avantrio"},
+    { name: "Dilanka Harshani", message: "Hello im studying at IIT"},
+    { name: "Hasani Dilhari", message: "Hello im working at JRC Software"}
+  ])
+
+
+  // const sayHello = () => {
+  //   console.log("hello");
+  // };
+
+  return(
+    <div className="app">
+      {/* <Tweet name="Rusiru Fernadno" message="Hello im working at Avantrio" />
+      <Tweet name="Dinu Senal" message="Hello im working at Avantrio" />
+      <Tweet name="Dilanka Harshani"  message="Hello im studying at IIT"/>
+      <Tweet name="Hasani Dilhari" message="Hello im working at JRC Software" /> */}
+
+      {
+        users.map(user =>(
+          <Tweet name={user.name} message={user.message} />
+        ))
+      }
     </div>
-  );
+  )
 }
+
+
 
 export default App;
